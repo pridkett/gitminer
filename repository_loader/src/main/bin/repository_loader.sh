375e84a8033f412923306a05df00bc5ad4a037b1
Patrick Wagstrom <patrick@wagstrom.net>
2011-08-12 15:39:10 -0400

    modifications to make repository_loader run from the command line

---
+++ repository_loader/src/main/bin/repository_loader.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Copyright 2011 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+CP=$( echo `dirname $0`/../lib/*.jar . | sed 's/ /:/g')
+
+# Find Java
+if [ "$JAVA_HOME" = "" ] ; then
+    JAVA="java -server"
+else
+    JAVA="$JAVA_HOME/bin/java -server"
+fi
+
+# Set Java options
+if [ "$JAVA_OPTIONS" = "" ] ; then
+    JAVA_OPTIONS="-Xms32M -Xmx512M"
+fi
+
+$JAVA $JAVA_OPTIONS -cp $CP edu.unl.cse.git.App $@
+
+# Return the program's exit code
+exit $?
