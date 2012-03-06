70a8a6688f780cce34c8f6b330db9ccdc0f5e5a6
Patrick Wagstrom <patrick@wagstrom.net>
2012-03-06 14:58:19 -0500

    missing file that resulted in broken build

---
+++ src/main/java/net/wagstrom/research/github/InvocationHandlerBase.java
@@ -0,0 +1,64 @@
+package net.wagstrom.research.github;
+
+import java.lang.reflect.Method;
+import java.net.ConnectException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class InvocationHandlerBase {
+    private Logger log;
+    protected static final long SLEEP_DELAY = 5000;
+    protected static final long MAX_SLEEP_DELAY = SLEEP_DELAY * 5;
+
+    protected long failSleepDelay = SLEEP_DELAY;
+    
+    public InvocationHandlerBase() {
+        log = LoggerFactory.getLogger(InvocationHandlerBase.class);    
+    }
+    
+    private void failSleep() {
+        try {
+            Thread.sleep(failSleepDelay);
+            failSleepDelay = failSleepDelay + SLEEP_DELAY;
+        } catch (InterruptedException e) {
+            log.error("Sleep interrupted",e);
+        }
+    }
+    
+    public abstract Object invoke(Object proxy, Method method, Object[] args)
+            throws Throwable;
+
+    protected Object handleInvocationException(Exception e, Object proxy, Method method, Object[] args) throws Throwable {
+        if (failSleepDelay > MAX_SLEEP_DELAY) {
+            log.error("Too many failures. Giving up and returning null");
+            log.error("method: {} args: {}", method, args);
+            return null;
+        }
+
+        if (e.getMessage().startsWith("API Rate Limit Exceeded for")) {
+            log.warn("Exceeding API rate limit -- Sleep for {}ms and try again", failSleepDelay);
+            failSleep();
+            return invoke(proxy, method, args);
+        } else if (e.getMessage().toLowerCase().indexOf("<title>server error - github</title>") != -1) {
+            log.warn("Received a server error from GitHub -- Sleep for {}ms and try again", failSleepDelay);
+            failSleep();
+            return invoke(proxy, method, args);
+        } else if (e.getMessage().trim().toLowerCase().equals("{\"error\":\"not found\"}")) {
+            log.warn("GitHub returned Not Found: Method: {}, Args: {}", method.getName(), args);
+            return null;
+        } else if (e.getCause() instanceof ConnectException) {
+            log.error("Connection exception: Method: {}, Args: {}", new Object[]{method.getName(), args, e});
+            failSleep();
+            return invoke(proxy, method, args);
+        } else if (e.getCause() != null && e.getCause().getCause() instanceof ConnectException) {
+            log.error("Connection exception (deep): Method: {}, Args: {}", new Object[]{method.getName(), args, e});
+            failSleep();
+            return invoke(proxy, method, args);         
+        }
+
+        log.error("Unhandled exception: Method: {} Args: {}", new Object[]{method.getName(), args, e});
+        failSleep();
+        return invoke(proxy, method, args); 
+    }
+}
