669cc2730e8d407b975f6d62efb601821ac31bd2
Patrick Wagstrom <patrick@wagstrom.net>
2012-05-01 12:45:48 -0400

    more of slow march to make static code checkers happy

--- src/main/java/net/wagstrom/research/github/v3/EventServiceV3.java
+++ src/main/java/net/wagstrom/research/github/v3/EventServiceV3.java
@@ -1,6 +1,5 @@
 package net.wagstrom.research.github.v3;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -16,22 +15,20 @@ import org.slf4j.LoggerFactory;
 public class EventServiceV3 extends EventService {
     private static final Logger log = LoggerFactory.getLogger(EventService.class); // NOPMD
     
-    public EventServiceV3(IGitHubClient ghc) {
+    public EventServiceV3(final IGitHubClient ghc) {
         super(ghc);
     }
 
-    protected <V> List<V> getAll(PageIterator<V> iterator) {
+    protected <V> List<V> getAll(final PageIterator<V> iterator) {
         List<V> elements = new ArrayList<V>();
         Collection<V> elements2 = null;
         try {
             while (iterator.hasNext()) {
                 elements2 = iterator.next();
-                log.warn("Elements2 size: {}", elements2.size());
                 elements.addAll(elements2);
             }
         } catch (NoSuchPageException pageException) {
             log.error("NoSuchPageException caught: ", pageException);
-            // throw pageException.getCause();
         }
         return elements;
     }
