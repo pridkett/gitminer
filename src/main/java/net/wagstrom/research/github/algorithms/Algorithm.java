af88a1a09991ad46d734f7e38033da587904f0e1
Patrick Wagstrom <patrick@wagstrom.net>
2012-06-19 11:18:55 -0400

    Corrected count of users associated with repos
    This resulted from the switch away from the original embedded germlin
    code to the new java pipes code. I wasn't creating a new pipe for
    each pipe and therefore was getting garbage results and an obscene
    number of users for most projects. This appears to have fixed it.
    
    I should stress, this DID NOT affect out results in an meaningful
    way. The queries for the analysis are all written in groovy and were
    not impacted by these changes. At some point, I'll need to make it so
    all the queries are done from a single source.
    
    Signed-off-by: Patrick Wagstrom <patrick@wagstrom.net>

--- src/main/java/net/wagstrom/research/github/algorithms/Algorithm.java
+++ src/main/java/net/wagstrom/research/github/algorithms/Algorithm.java
@@ -1,9 +1,12 @@
 package net.wagstrom.research.github.algorithms;
 
 import net.wagstrom.research.github.BlueprintsDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Algorithm {
     protected BlueprintsDriver driver;
+    private final static Logger log = LoggerFactory.getLogger(Algorithm.class); // NOPMD
     
     public Algorithm(final BlueprintsDriver driver) {
         this.driver = driver;
